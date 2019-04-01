09ae3194509181d0d395b34ffe19cc128d911aad
Squartzz <49129411+Squartzz@users.noreply.github.com>
2019-04-01 19:57:58 +0200

    Update README.md and install.sh to use SSL.

--- README.md
+++ README.md
@@ -7,12 +7,17 @@ INSTALLATION INSTRUCTIONS :
 
 Open a terminal :
   git clone https://github.com/Squartzz/soundscape-installer.git;
-  
+
   cd soundscape-installer;
-  
+
   sudo chmod +x install.sh;
-  
+
   ./install.sh
+
+  If you want to use https:
+
+    - You need a publicy resolvable DNS record ;
+    - You need the port 80 and 443
 
 Follow the instruction, and you are Done !!!
 -------------------------------------------------
--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo "Downloading Soundscape .";
 sleep 2;
+// Downloading Soundscape official ;
 wget -O soundscape https://github.com/soundscapecloud/soundscape/raw/master/soundscape-linux-amd64;
 sudo chmod +x soundscape;
 echo " Choose your music folder (ex : /home/user/Music/Soundscape): "
 read a;
 echo "Installing Soundscape"
+// Authorize soundscape to use port 80 and 443 without admin permission. ;
 sudo setcap cap_net_bind_service=+ep soundscape;
 mkdir $a;
 echo "Choose your password : ";
@@ -13,9 +15,19 @@ read b;
 echo "$b" > $a/.authsecret;
 echo "What is Your Host ?";
 read c;
-echo "#!/bin/bash
+echo "Do you want to use https (y or n)";
+read d;
+if [ $d = y ]
+then
+  echo "#!/bin/bash
 
-./soundscape --http-host $c --http-username $USER --data-dir $a
+./soundscape --http-host $c --http-username $USER --data-dir $a --letsencrypt
 " > start.sh;
+else
+  echo "#!/bin/bash
+
+  ./soundscape --http-host $c --http-username $USER --data-dir $a
+  " > start.sh;
+fi;
 echo "Done,use ./start.sh to start soundscape";
 chmod +x start.sh
